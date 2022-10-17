0028829eb2429f26adfd0fd8c4f354d7f17c094a
Joszko <jakub.rezler96@gmail.com>
2022-10-17 02:36:57 +0200

    create home page - add musical news

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "../style/pages-style/home.css";
+import tiffany from "../images/Tiffany's.jpg";
 
 const Home = () => {
-	return <div>a</div>;
+	return (
+		<div>
+			<div className="musical-container">
+				<img
+					src={tiffany}
+					alt="logo of musical Tiffany's Lunatic Boomerang"
+					className="tiffany-img"
+				/>
+				<div className="musical-text-content">
+					<h2>Tiffany's Lunatic Boomerang</h2>
+					<p>
+						Musical "Tiffany's Lunatic Boomerang" which I've created with music
+						producer{" "}
+						<a href="https://www.jaime-albadalejo.com/">Jaime Albadalejo </a>
+						will be realased soon! It's psychological musical/thriller in
+						spanish and english version. Recordings of music orchestrated by my
+						self have just started. Stay tuned!
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
