e2deaf252449480e29efc724cfb4f9dab5771251
Joszko <jakub.rezler96@gmail.com>
2022-10-18 00:57:13 +0200

    add links to footer

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -8,11 +8,21 @@ const Footer = () => {
 				2022 Jakub Rezler || &copy; All rights reserved
 			</span>
 			<div className="icons-container-footer">
-				<i className="fa-brands fa-soundcloud"></i>
-				<i className="fa-brands fa-youtube"></i>
-				<i className="fa-brands fa-linkedin"></i>
-				<i className="fa-brands fa-twitter"></i>
-				<i className="fa-brands fa-spotify"></i>
+				<a href="https://soundcloud.com/jakub-rezler-883804903">
+					<i className="fa-brands fa-soundcloud"></i>
+				</a>
+				<a href="https://www.youtube.com/user/Reziok3">
+					<i className="fa-brands fa-youtube"></i>
+				</a>
+				<a href="https://www.linkedin.com/in/jakub-rezler-7b08211ab">
+					<i className="fa-brands fa-linkedin"></i>
+				</a>
+				<a href="https://twitter.com/Jakub_Rezler">
+					<i className="fa-brands fa-twitter"></i>
+				</a>
+				<a href="https://open.spotify.com/artist/7poqdc8Z8CksjZhleR5fbK?si=JUumspV_TGeaKHzgn6wLgg">
+					<i className="fa-brands fa-spotify"></i>
+				</a>
 			</div>
 		</div>
 	);
