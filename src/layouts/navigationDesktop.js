422301eb8182606401685137a4bfb256cd0e3e80
Joszko <jakub.rezler96@gmail.com>
2022-10-18 01:19:43 +0200

    change RWD in desktop navigation

--- src/layouts/navigationDesktop.js
+++ src/layouts/navigationDesktop.js
@@ -27,6 +27,23 @@ const NavigationDesktop = () => {
 					Contact
 				</NavLink>
 			</div>
+			<div className="icons-container-navigation">
+				<a href="https://soundcloud.com/jakub-rezler-883804903">
+					<i className="fa-brands fa-soundcloud"></i>
+				</a>
+				<a href="https://www.youtube.com/user/Reziok3">
+					<i className="fa-brands fa-youtube"></i>
+				</a>
+				<a href="https://www.linkedin.com/in/jakub-rezler-7b08211ab">
+					<i className="fa-brands fa-linkedin"></i>
+				</a>
+				<a href="https://twitter.com/Jakub_Rezler">
+					<i className="fa-brands fa-twitter"></i>
+				</a>
+				<a href="https://open.spotify.com/artist/7poqdc8Z8CksjZhleR5fbK?si=JUumspV_TGeaKHzgn6wLgg">
+					<i className="fa-brands fa-spotify"></i>
+				</a>
+			</div>
 		</nav>
 	);
 };
