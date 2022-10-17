fd878367a7f8b9e889f27d2d931d2c67b1f7ee3b
Joszko <jakub.rezler96@gmail.com>
2022-10-17 03:17:17 +0200

    add split lines in home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,19 @@ import Musical from "../components/home-content-musical";
 const Home = () => {
 	return (
 		<div>
+			<h1
+				style={{
+					margin: "50px 0 10px 20px",
+					fontSize: "25px",
+					color: "white",
+					textTransform: "uppercase",
+				}}
+			>
+				News
+			</h1>
+			<div className="home-split-line"></div>
 			<Musical />
+			<div className="home-split-line"></div>
 		</div>
 	);
 };
