7847ecec39a14d9e06d1ebc3abfb8b75aecc6d9a
Joszko <jakub.rezler96@gmail.com>
2022-10-18 00:20:36 +0200

    add website logo to desktop navigation

--- src/layouts/navigationDesktop.js
+++ src/layouts/navigationDesktop.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../style/css/navigationDesktop.css";
+import trebleClef from "../images/cleff.png";
 
 const NavigationDesktop = () => {
 	return (
 		<nav className="nav-bar-desktop">
-			<NavLink to="/" end className="nav-btn-desktop">
-				Home
-			</NavLink>
-			<NavLink to="/music" className="nav-btn-desktop">
-				Music
-			</NavLink>
-			<NavLink to="/credits" className="nav-btn-desktop">
-				Credits
-			</NavLink>
-			<NavLink to="/offer" className="nav-btn-desktop">
-				Offer
-			</NavLink>
-			<NavLink to="/contact" className="nav-btn-desktop">
-				Contact
-			</NavLink>
+			<div className="jrcompose-logo-container">
+				<img src={trebleClef} alt="treble-clef" className="treble-clef-img" />
+				<span className="jrcompose-span">JR Compose</span>
+			</div>
+			<div className="desktop-nav-links">
+				<NavLink to="/" end className="nav-btn-desktop">
+					Home
+				</NavLink>
+				<NavLink to="/music" className="nav-btn-desktop">
+					Music
+				</NavLink>
+				<NavLink to="/credits" className="nav-btn-desktop">
+					Credits
+				</NavLink>
+				<NavLink to="/offer" className="nav-btn-desktop">
+					Offer
+				</NavLink>
+				<NavLink to="/contact" className="nav-btn-desktop">
+					Contact
+				</NavLink>
+			</div>
 		</nav>
 	);
 };
