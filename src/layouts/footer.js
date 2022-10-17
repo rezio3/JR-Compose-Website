625a360b71140819ee0a40ee0cbd32914b850e44
Joszko <jakub.rezler96@gmail.com>
2022-10-18 00:50:29 +0200

    footer design

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -4,7 +4,16 @@ import "../style/css/footer.css";
 const Footer = () => {
 	return (
 		<div className="footer-container">
-			<span className="footer-span">Copyright</span>
+			<span className="footer-span">
+				2022 Jakub Rezler || &copy; All rights reserved
+			</span>
+			<div className="icons-container-footer">
+				<i className="fa-brands fa-soundcloud"></i>
+				<i className="fa-brands fa-youtube"></i>
+				<i className="fa-brands fa-linkedin"></i>
+				<i className="fa-brands fa-twitter"></i>
+				<i className="fa-brands fa-spotify"></i>
+			</div>
 		</div>
 	);
 };
