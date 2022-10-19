188cafc1a3f52b18380e30e87d7138e8ec378ff3
Joszko <jakub.rezler96@gmail.com>
2022-10-19 23:55:59 +0200

    change offer page - change font and add order buttons

--- src/pages/offer.js
+++ src/pages/offer.js
@@ -3,13 +3,23 @@ import "../style/css/offer.css";
 import OfferGameMusic from "../components/offer-game-music";
 import OfferFilmMusic from "../components/offer-film-music";
 import OfferTheaterMusic from "../components/offer-theater-music";
+import { NavLink } from "react-router-dom";
 
 const Offer = () => {
 	return (
-		<div>
+		<div className="order-container">
 			<OfferGameMusic />
+			<NavLink to="/contact" className="nav-btn-desktop">
+				<button className="order-btn">Order</button>
+			</NavLink>
 			<OfferFilmMusic />
+			<NavLink to="/contact" className="nav-btn-desktop">
+				<button className="order-btn">Order</button>
+			</NavLink>
 			<OfferTheaterMusic />
+			<NavLink to="/contact" className="nav-btn-desktop">
+				<button className="order-btn">Order</button>
+			</NavLink>
 		</div>
 	);
 };
